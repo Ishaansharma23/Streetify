48a011bb53fb70413ff3af201aabc9edc4aed015
Ishaansharma23 <sharmaishaan0009@gmail.com>
2025-04-12 13:11:48 +0530

    Animation changes

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -4,12 +4,11 @@ import { Link as ScrollLink } from 'react-scroll'
 
 const Navbar = () => {
     return (
-        <nav className="bg-black text-white p-4 shadow-md fixed top-0 left-0 w-full z-50">
+        <nav className="bg-black text-white p-4 shadow-md fixed top-0 left-0 w-full z-50 ">
             <div className="container mx-auto flex justify-between items-center">
 
-                {/* <h1 className="text-3xl font-bold text-white py-5 hover:text-orange-500 transition-all duration-500 transform hover:rotate-360">Streetify🍽️</h1> */}
                 <ScrollLink to='home' smooth={true} duration={500} className="cursor-pointer text-3xl font-bold text-white py-5 hover:text-orange-500 transition-all duration-500 transform hover:rotate-360">
-                    Streetify🍽️
+                  Streetify🍽️
                 </ScrollLink>
 
                 <div className="flex space-x-6">
@@ -23,6 +22,7 @@ const Navbar = () => {
                     <ScrollLink to='Download App' smooth={true} duration={500} offset={-80} className='py-5 text-white hover:text-orange-500 transition-all duration-500 transform hover:rotate-360'>
                         Download App
                     </ScrollLink>
+
 
                     <Link to="/contact" className='py-5 text-white hover:text-orange-500 transition-all duration-500 transform hover:rotate-360'>
                         Contact Us
